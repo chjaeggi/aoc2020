348041eb8b52553c30ddbf95b23bfd5cd634acee
Christian Jaeggi <chr.jaeggi@gmail.com>
2020-12-03 16:46:48 +0100

    day3 solutions

--- src/main/kotlin/chjaeggi/Day3.kt
+++ src/main/kotlin/chjaeggi/Day3.kt
@@ -6,15 +6,15 @@ class Day3(private val input: List<String>) {
         it.repeat((input.size / input[0].length) * 10)
     }
 
-    private fun findTrees(treeMap: List<String>, x: Int, y: Int): Int {
+    private fun findTrees(x: Int, y: Int): Int {
         var right = 0
         var down = 0
         var result = 0
 
-        while (down < treeMap.size - 1) {
+        while (down < repeatedMap.size - 1) {
             right += x
             down += y
-            if (treeMap[down][right] == '#') {
+            if (repeatedMap[down][right] == '#') {
                 result += 1
             }
         }
@@ -22,12 +22,12 @@ class Day3(private val input: List<String>) {
     }
 
     fun solvePart1(): Int {
-        return findTrees(repeatedMap, 3, 1)
+        return findTrees(3, 1)
     }
 
     fun solvePart2(): Int {
-        return findTrees(repeatedMap, 1, 1) * findTrees(repeatedMap, 3, 1) *
-                findTrees(repeatedMap, 5, 1) * findTrees(repeatedMap, 7, 1) *
-                findTrees(repeatedMap, 1, 2)
+        return findTrees(1, 1) * findTrees(3, 1) *
+                findTrees(5, 1) * findTrees(7, 1) *
+                findTrees(1, 2)
     }
 }
